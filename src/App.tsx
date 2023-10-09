1386193b690c118434557a5ebc8528bbeb8a3a7c
John Doe <mitka_j@yahoo.com>
2023-10-09 19:05:00 +0200

    work in progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ErrorPage, Home, Signup, Login } from "./pages";
+import Sidebar from "./components/Sidebar/Sidebar";
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <BrowserRouter>
+      <Sidebar>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </Sidebar>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
