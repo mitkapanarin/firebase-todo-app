e56f89946394fd54c36ac2fb16a69185ac2f3b05
mitkapanarin <mitka_j@yahoo.com>
2023-11-21 22:49:33 +0100

    added file upload

--- src/store/index.ts
+++ src/store/index.ts
@@ -13,49 +13,47 @@ import {
   useSetNewPassWordMutation,
   useUpdateUserProfileMutation,
 } from "./API/userAuthAPI";
+import { storageAPI } from "./API/storageAPI";
 
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
-
 const persistConfig = {
   key: "root",
   storage,
 };
-
 const persistedSystemReducer = persistReducer(
   persistConfig,
   sysmtemSlice.reducer
 );
-
 const persistedUserReducer = persistReducer(
   persistConfig,
   userDataSlice.reducer
 );
-
 export const store = configureStore({
   reducer: {
     system: persistedSystemReducer,
     user: persistedUserReducer,
     [userAuthAPI.reducerPath]: userAuthAPI.reducer,
     [taskAPI.reducerPath]: taskAPI.reducer,
+    [storageAPI.reducerPath]: storageAPI.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
-    }).concat(userAuthAPI.middleware, taskAPI.middleware),
+    }).concat(
+      userAuthAPI.middleware,
+      taskAPI.middleware,
+      storageAPI.middleware
+    ),
 });
 
 export const persistedStore = persistStore(store);
-
 setupListeners(store.dispatch);
-
 export type RootState = ReturnType<typeof store.getState>;
-
 export {
   // system settings
   resetSystem,
   themeSwitch,
-
   // user auth
   useEmailSignupMutation,
   useEmailLoginMutation,
@@ -64,7 +62,6 @@ export {
   useSendResetPassWordEmailMutation,
   useSetNewPassWordMutation,
   useUpdateUserProfileMutation,
-
   // user auth slice
   loginSuccess,
   logoutSuccess,
