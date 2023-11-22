0f91fde19286e876917f8cdbca492fd1e65cfc07
mitkapanarin <mitka_j@yahoo.com>
2023-11-22 14:41:22 +0100

    initialState name removed

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -9,8 +9,7 @@ import { IUserSignInData } from "../types/interface";
 const Login = () => {
   const initialState: IUserSignInData = {
     email: "khondokoralam@gmail.com",
-    password: "1234567",
-    name: ""
+    password: "1234567"
   };
 
   const navigate = useNavigate();
